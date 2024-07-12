9a341f0400096b8dbf3f3f3f953c068b12dd8757
Raghavendra Meena <92488385+raghavendram3@users.noreply.github.com>
2024-07-13 00:18:56 +0200

    Create get_images.php

---
+++ get_images.php
@@ -0,0 +1,7 @@
+<?php
+$images = glob('images/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+shuffle($images);
+$images = array_map('basename', $images);
+header('Content-Type: application/json');
+echo json_encode($images);
+?>
