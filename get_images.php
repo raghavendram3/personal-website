b48e71af64551728925447b88d4a25256a00dff6
Raghavendra Meena <92488385+raghavendram3@users.noreply.github.com>
2024-07-13 00:34:10 +0200

    Update get_images.php

--- get_images.php
+++ get_images.php
@@ -1,5 +1,5 @@
 <?php
-$images = glob('/images/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+$images = glob("/images/*.{jpg,jpeg,png,gif}", GLOB_BRACE);
 shuffle($images);
 $images = array_map('basename', $images);
 header('Content-Type: application/json');
