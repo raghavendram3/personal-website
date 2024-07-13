6321043a6d81a98a35bd3da6b859f58395a6cec9
Raghavendra Meena <92488385+raghavendram3@users.noreply.github.com>
2024-07-13 05:17:09 +0200

    Update and rename get_images.php to images.json

--- get_images.php
+++
@@ -1,8 +0,0 @@
-<?php
-$directory = 'images';
-$images = glob($directory . '/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-shuffle($images);
-$images = array_map('basename', $images);
-header('Content-Type: application/json');
-echo json_encode($images);
-?>
